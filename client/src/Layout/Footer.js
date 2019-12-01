439a8526cef8b8c71f31484999ccd56d34bec7b2
luispadre <52799375+luispadre@users.noreply.github.com>
2019-11-30 20:50:28 -0600

    Material

--- client/src/Layout/Footer.js
+++ client/src/Layout/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Footer() {
-    return(
-        <>
-          Footer
-        </>
-    )
+    return(<>
+<div class="footer-copyright" style={{backgroundColor: "rgb(62, 69, 81)", color:" rgb(255, 255, 255)"}}>
+  <div class="container-fluid"> 2020 Luis Antonio Padre Garcia y Jose Antonio Padre Garcia </div>
+  </div>
+     </> )
+
 }
--- client/src/Pages/Cargos/index.jsx
+++ client/src/Pages/Cargos/index.jsx
@@ -1,5 +1,76 @@
 import React from 'react'
+import { Card, Col, Row } from 'antd';
+import { Tag } from 'antd';
 
-export default function Cargos(){
-return(<>Cargos</>)
+
+const { Meta } = Card;
+export default function Cargos() {
+    return (<>
+        <div style={{ background: '#ECECEC', padding: '30px' }}>
+        <Tag color="#f50">Delegación</Tag>
+
+
+            <Row gutter={16}>
+                <Col span={8}>
+                <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={8}>
+
+                    <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+            </Row>
+            <Tag color="#A980E2">Servicio y Vigilancia</Tag>
+            <Row gutter={16}>
+                <Col span={8}>
+                <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={8}>
+
+                    <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    </>)
 }
